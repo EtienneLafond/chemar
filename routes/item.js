a61504f8a273874f9f8b821e897b28db63869636
Liam Osler <liam.osler@dal.ca>
2023-03-08 17:20:25 -0400

    add comments to item.js, refactor for better error handling

--- routes/item.js
+++ routes/item.js
@@ -4,33 +4,42 @@ var fs = require('fs');
 
 
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.redirect('/catalog');
-});
-
 router.get('/:id', function(req , res){
-  var molfiles = fs.readdirSync('./public/molfiles/')
-  let data = JSON.parse(fs.readFileSync('./public/catalog/catalog.json', "utf8"));
-
-  for(let i = 0; i < molfiles.length; i++){
-    molfiles[i] = molfiles[i].replace('.mol', '');
-  }
-  if(molfiles.includes(req.params.id)){
-    if(!data[req.params.id].hasOwnProperty('name')){
-      data[req.params.id].name = 'No name in catalog';
-      data[req.params.id].formula = 'No formula in catalog';
+  
+  //Read the molfiles directory and store the names of the files in an array
+  let molfiles = fs.readdirSync('./public/molfiles/')
+  
+  //Read the contents of the catalogData.json file and store the contents in a variable called data
+  let catalogData = JSON.parse(fs.readFileSync('./public/catalog/catalog.json', "utf8"));
+  
+  //Check if the array includes a file with the same name as the id parameter
+  if(molfiles.includes(req.params.id + '.mol')){
+    
+    //If the file exists, read the file and store the contents in a variable called molfile
+    let molfile = fs.readFileSync('./public/molfiles/'+req.params.id+'.mol', 'utf8');
+    
+    let moleculeName = 'No name in catalog';
+    let moleculeFormula = 'No formula in catalog';
+    //Check if the catalogData object has a property with the same name as the id parameter (needs to correspond to file name)
+    if(catalogData[req.params.id]){
+      //If the property does exist, set the name and formula properties to 'No name in catalog' and 'No formula in catalog'
+      moleculeName = catalogData[req.params.id].name;
+      moleculeFormula = catalogData[req.params.id].formula;
     }
-    var molfile = fs.readFileSync('./public/molfiles/'+req.params.id+'.mol', 'utf8');
+    
+    //Render the item page with the id as the item argument and the molfile as the molfile argument
     res.render('item', {
       title: 'Molecule Viewer: '+ req.params.id, 
       item: req.params.id, 
       molfile: molfile,
-      name: data[req.params.id].name,
-      formula: data[req.params.id].formula,
+      name: moleculeName,
+      formula: moleculeFormula,
     });
-  }else{
-    res.render('error', {title: 'Error', message: 'Molecule not found', error: {status: 404, stack: ''}});
+  }
+  
+  //If the file does not exist, render the error page:
+  else{
+    res.render('error', {title: 'Error', message: 'Molecule not found', error: {status: 404}});
   }
 });
 
