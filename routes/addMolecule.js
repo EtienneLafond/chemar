6829f96a3b00d0e05215f3b80fb22ca353a7246c
Etienne Lafond <et998726@dal.ca>
2023-03-09 17:48:20 -0400

    Implemented post function in addMolecule.js which creates the new .mol file in ./public/molefiles from the form data

--- routes/addMolecule.js
+++ routes/addMolecule.js
@@ -6,12 +6,28 @@ router.get('/', function(req, res, next) {
     res.render('addMolecule', {title: 'Add New Molecule'});
 });
 
-// Submit button
+// Post request when clicking submit button
 router.post('/', (req, res) => {
     console.log(req.body.name);
     console.log(req.body.formula);
     console.log(req.body.fileName);
     console.log(req.body.preview);
+
+    // Assign values from form to variables
+    var molName = req.body.name;
+    var molFormula = req.body.formula;
+    var fileName = req.body.fileName;
+    var molFileContent = req.body.preview;
+
+    // Create new mol file in ./public/molefiles/
+    fs.writeFile('./public/molfiles/'+fileName, molFileContent, function (err) {
+        if (err) throw err;
+        console.log('New melecule created');
+    });
+
+    // Return to catalog page
+    res.redirect('/catalog');
+
    // const selectedFile = req.getElementById("file").files[0];
     //res.send('POST request to the homepage')
 
