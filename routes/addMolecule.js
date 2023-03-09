39c31b7e7e966a33d7aabc62286506d541ab8253
Etienne Lafond <et998726@dal.ca>
2023-03-09 17:15:50 -0400

    Implemented script in addMolecule.ejs to read and preview the selected .mol file

--- routes/addMolecule.js
+++ routes/addMolecule.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 router.get('/', function(req, res, next) {
     res.render('addMolecule', {title: 'Add New Molecule'});
@@ -7,8 +8,17 @@ router.get('/', function(req, res, next) {
 
 // Submit button
 router.post('/', (req, res) => {
-    console.log(req.body.file);
-    res.send('POST request to the homepage')
+    console.log(req.body.name);
+    console.log(req.body.formula);
+    console.log(req.body.fileName);
+    console.log(req.body.preview);
+   // const selectedFile = req.getElementById("file").files[0];
+    //res.send('POST request to the homepage')
+
+    //var molfile = fs.readFileSync(req.body.fileName, 'utf8');
+    //console.log(molfile);
+
+    //res.send('POST request to the homepage')
 });
 
 module.exports = router;
