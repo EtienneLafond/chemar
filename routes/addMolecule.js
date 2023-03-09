49e580b2f2fbbc1848403920d1b2bc8ec060b793
Etienne Lafond <et998726@dal.ca>
2023-03-09 10:23:49 -0400

    Added post route to addMolecule.js

--- routes/addMolecule.js
+++ routes/addMolecule.js
@@ -5,4 +5,10 @@ router.get('/', function(req, res, next) {
     res.render('addMolecule', {title: 'Add New Molecule'});
 });
 
+// Submit button
+router.post('/', (req, res) => {
+    console.log(req.body.file);
+    res.send('POST request to the homepage')
+});
+
 module.exports = router;
