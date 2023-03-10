ca479b9722a29b3e55d18c25102cfbc43b3fdb9c
Etienne Lafond <et998726@dal.ca>
2023-03-10 12:45:06 -0400

    Implemented the check if the mol file already exists

--- routes/addMolecule.js
+++ routes/addMolecule.js
@@ -6,12 +6,21 @@ router.get('/', function(req, res, next) {
     res.render('addMolecule', {title: 'Add New Molecule'});
 });
 
+router.post('/test', (req, res) => {
+    var molfiles = fs.readdirSync('./public/molfiles/')
+    var testCheck = "702.mol"
+
+    var fileIsPresent = molfiles.includes(testCheck);
+
+    res.send(`files: ${JSON.stringify(molfiles)} \n chck: ${testCheck} \n ispresent: ${fileIsPresent}`);    
+});
+
 // Post request when clicking submit button
 router.post('/', (req, res) => {
-    console.log(req.body.name);
-    console.log(req.body.formula);
-    console.log(req.body.fileName);
-    console.log(req.body.preview);
+    //console.log(req.body.name);
+    //console.log(req.body.formula);
+    //console.log(req.body.fileName);
+    //console.log(req.body.preview);
 
     // Assign values from form to variables
     var molName = req.body.name;
@@ -19,22 +28,23 @@ router.post('/', (req, res) => {
     var fileName = req.body.fileName;
     var molFileContent = req.body.preview;
 
-    // Create new mol file in ./public/molefiles/
-    fs.writeFile('./public/molfiles/'+fileName, molFileContent, function (err) {
-        if (err) throw err;
-        console.log('New melecule created');
-    });
+    var molfiles = fs.readdirSync('./public/molfiles/');
+    var fileIsPresent = molfiles.includes(fileName);
+
+    // Check if the molecule already exists
+    if (fileIsPresent) {
+        console.log('This molecule already exists.');
+    }
+    else {
+        // Create new mol file in ./public/molefiles/
+        fs.writeFile('./public/molefiles/'+fileName, molFileContent, function (err) {
+            if (err) throw err;
+                console.log('New molecule created');
+        });
+    }    
 
     // Return to catalog page
     res.redirect('/catalog');
-
-   // const selectedFile = req.getElementById("file").files[0];
-    //res.send('POST request to the homepage')
-
-    //var molfile = fs.readFileSync(req.body.fileName, 'utf8');
-    //console.log(molfile);
-
-    //res.send('POST request to the homepage')
 });
 
 module.exports = router;
