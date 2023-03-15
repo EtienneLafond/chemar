783d006b06d94069def7012fe98ea945f7584478
Liam Osler <osler@cs.dal.ca>
2023-03-15 17:43:54 -0300

    Revert "Merge branch 'Develop-Experimental' into 'CHEM-72'"
    This reverts commit a2390465a1e51717d3dd12923a04474213606e8b

--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var fs = require('fs');
+var fs = require('fs'); 
 
-/* GET users listing. */
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  let numScenes = fs.readdirSync("./public/scenes/");
-  let scenes = JSON.parse(fs.readFileSync("./public/scenes/scenes.json", "utf8"));
-  
-  res.render('admin', {title: 'Admin Page', scenes: scenes, numScenes : numScenes});
+  res.redirect('/catalog');
 });
-
-module.exports = router;
