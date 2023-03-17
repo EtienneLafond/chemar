3499059e8eb7c8f1725d091ce76065c66fb15598
Jacob <jacobsehl@tfa60.wpa.dal.ca>
2023-03-17 11:13:00 -0300

    Added preview modal and edit/delete buttons

--- routes/catalog.js
+++ routes/catalog.js
@@ -5,7 +5,8 @@ var fs = require('fs');
 /* GET home page. */
 router.get('/', function(req, res, next) {
     const molfiles = './public/molfiles/';
-    res.render('catalog', { title: 'Catalog', list: fs.readdirSync(molfiles)});
+    let molecule = JSON.parse(fs.readFileSync("./public/catalog/catalog.json"));
+    res.render('catalog', { title: 'Catalog', list: fs.readdirSync(molfiles), mol: molecule});
 });
 
 module.exports = router;
